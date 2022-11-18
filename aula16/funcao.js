9a9c5ba5da9699d242d8c7cbd4d69d3caa45b6d6
birajnior <ubiratanbernardo@gmail.com>
2022-11-18 19:36:31 -0300

    Adicionando aula 16 funções

---
+++ aula16/funcao.js
@@ -0,0 +1,12 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+let res = parimp(18)
+
+console.log(parimp(15));
+console.log(res);
